c44738a9588ff692672732ca50dd02941f7946f0
RoR-Salman <salman.shahzad@viralsquare.org>
2022-11-03 14:05:43 +0500

    refactored production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "meme support",
+    :user_name            => "memecustomercare00@gmail.com",
     :password             => "qhxziofqhzmfaasq",
     :authentication       => "plain",
     :enable_starttls_auto => true
