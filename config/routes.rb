d6001f411190fceaed4d268ba22aa692219b696f
RoR-Salman <salman.shahzad@viralsquare.org>
2022-11-03 12:55:09 +0500

    added root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "dashboard#dashboard"
   namespace :api do
     namespace :v1 do
       resources :users do
