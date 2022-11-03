4ffc6b08e8fbb4418dc2395170df9648d68d2fe8
Umair <umairchaudhary6161@gmail.com>
2022-11-03 18:05:29 +0500

    bug_fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # mount_uploader :avatar, AvatarUploader
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username
+  # validates :username, presence: true
   # validates :password,
   #           length: { minimum: 6 },
   #           if: -> { new_record? || !password.nil? }
