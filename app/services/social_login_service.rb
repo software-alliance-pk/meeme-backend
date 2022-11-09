7c736ae46359060a5b9b1ccdf300088509021f4a
Umair <umairchaudhary6161@gmail.com>
2022-11-09 17:18:40 +0500

    file pushed

--- app/services/social_login_service.rb
+++ app/services/social_login_service.rb
@@ -42,7 +42,7 @@ class SocialLoginService
     json_response = JSON.parse(response.body)
     user = create_user(json_response['email'], json_response['sub'], json_response)
     profile_image=  user.profile_image.attached? ? user.profile_image.blob.url : ''
-    token = JsonWebTokenService.encode(user_id: user.id)  
+    token = JsonWebTokenService.encode(user_id: user.id)
     user.verification_tokens.create(token: token,user_id: user.id)
 
     [user, token, profile_image]
