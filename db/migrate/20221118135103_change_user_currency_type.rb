ce3d2b95e5a19ac33823d7bdd62f0590370f1ac4
Umair <umairchaudhary6161@gmail.com>
2022-11-18 19:02:57 +0500

    bug fix

--- db/migrate/20221118135103_change_user_currency_type.rb
+++ db/migrate/20221118135103_change_user_currency_type.rb
@@ -1,6 +1,5 @@
 class ChangeUserCurrencyType < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users,:coins
-
+    # remove_column :users,:coins
   end
 end
--- db/migrate/20221118135252_add_currency_attribute.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrencyAttribute < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users,:coins, :integer,default: 0
-  end
-end
