64c3fdee727c1d25271f81251c132e6e2ad7c838
Umair <umairchaudhary6161@gmail.com>
2022-12-05 20:49:59 +0500

    bug fix end was missing

--- app/controllers/api/v1/followers_controller.rb
+++ app/controllers/api/v1/followers_controller.rb
@@ -6,11 +6,11 @@ class Api::V1::FollowersController < Api::V1::ApiController
 
   # GET /users
   def index
-    @user_followers = @current_user.followers.where(is_following: true,status: "added").paginate(page: params[:page], per_page: 25)
+    @user_followers = @current_user.followers.where(is_following: true, status: "added").paginate(page: params[:page], per_page: 25)
     if @user_followers.present?
       render json: { followers_count: @user_followers.count, followers: @user_followers }, status: :ok
     else
-      render json: { followers: @user_followers  }, status: :not_found
+      render json: { followers: @user_followers }, status: :not_found
     end
   end
 
@@ -18,12 +18,12 @@ class Api::V1::FollowersController < Api::V1::ApiController
     @user_followers = @current_user.pending_friend_request.paginate(page: params[:page], per_page: 25)
     if @user_followers.present?
     else
-      render json: { followers: @user_followers  }, status: :ok
+      render json: { followers: @user_followers }, status: :ok
     end
   end
 
   def create
-    @follower = Follower.new(user_id: @current_user.id, is_following: true, follower_user_id: params[:follower_user_id],status: 'added')
+    @follower = Follower.new(user_id: @current_user.id, is_following: true, follower_user_id: params[:follower_user_id], status: 'added')
     if @follower.save
       render json: { user: @current_user, follower: @follower, message: "#{@current_user.username} and #{User.find_by(id: @follower.follower_user_id).username} are now friends" }, status: :ok
     else
@@ -32,18 +32,19 @@ class Api::V1::FollowersController < Api::V1::ApiController
   end
 
   def send_a_follow_request_to_user
-    @follower= Follower.where(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id],status: 'pending')
+    @follower = Follower.where(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id], status: 'pending')
     if @follower.present?
-      render json: { message: "Request already sent a request"}, status: :ok
-    @follower = Follower.new(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id],status: 'pending')
-    if @follower.save
-      render json: { user: @current_user, follower: @follower, message: "#{@current_user.username} sent a request to #{User.find_by(id: @follower.follower_user_id).username} " }, status: :ok
-    else
-      @follower = Follower.new(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id],status: 'pending')
+      render json: { message: "Request already sent a request" }, status: :ok
+      @follower = Follower.new(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id], status: 'pending')
       if @follower.save
         render json: { user: @current_user, follower: @follower, message: "#{@current_user.username} sent a request to #{User.find_by(id: @follower.follower_user_id).username} " }, status: :ok
       else
-        render_error_messages(@follower)
+        @follower = Follower.new(user_id: @current_user.id, is_following: false, follower_user_id: params[:follower_user_id], status: 'pending')
+        if @follower.save
+          render json: { user: @current_user, follower: @follower, message: "#{@current_user.username} sent a request to #{User.find_by(id: @follower.follower_user_id).username} " }, status: :ok
+        else
+          render_error_messages(@follower)
+        end
       end
     end
   end
@@ -51,13 +52,13 @@ class Api::V1::FollowersController < Api::V1::ApiController
   def update_follower
     @follower = Follower.find_by(user_id: params[:follower_user_id], follower_user_id: @current_user.id)
     if @follower.present?
-      if @follower.is_following.to_s==params[:is_following] && @follower.added?
+      if @follower.is_following.to_s == params[:is_following] && @follower.added?
         render json: { message: "User unfriend his follower" }, status: :ok
-      elsif @follower.is_following.to_s==params[:is_following] && @follower.pending?
+      elsif @follower.is_following.to_s == params[:is_following] && @follower.pending?
         @follower.destroy
         render json: { message: "User removed from pending" }, status: :ok
       else
-        @follower.update(is_following: true,status: 'added')
+        @follower.update(is_following: true, status: 'added')
         render json: { message: "User added this follower", request: @follower }, status: :ok
       end
     else
@@ -80,9 +81,9 @@ class Api::V1::FollowersController < Api::V1::ApiController
   end
 
   def show_people
-    users=@current_user.followers.where(is_following: false).pluck(:follower_user_id)
-    users=users.push(@current_user.id)
-    users=User.where.not(id: users).paginate(page: params[:page], per_page: 25)
+    users = @current_user.followers.where(is_following: false).pluck(:follower_user_id)
+    users = users.push(@current_user.id)
+    users = User.where.not(id: users).paginate(page: params[:page], per_page: 25)
     if users.present?
       render json: { people_count: users.count, people_not_added: users }, status: :ok
     else
@@ -94,7 +95,7 @@ class Api::V1::FollowersController < Api::V1::ApiController
 
   def find_user
     unless (@user = User.find_by(id: params[:follower_user_id]))
-      return render json: { message: 'User Not found' },status: :not_found
+      return render json: { message: 'User Not found' }, status: :not_found
     end
   end
 
@@ -103,7 +104,7 @@ class Api::V1::FollowersController < Api::V1::ApiController
   end
 
   def not_following_himself
-    return render json: { message: 'You cannot follow yourself' } if  (@current_user.id == params[:follower_user_id].to_i)
+    return render json: { message: 'You cannot follow yourself' } if (@current_user.id == params[:follower_user_id].to_i)
   end
 
 end
---
+++ app/models/user_store.rb
@@ -0,0 +1,3 @@
+class UserStore < ApplicationRecord
+  has_many :users
+end
