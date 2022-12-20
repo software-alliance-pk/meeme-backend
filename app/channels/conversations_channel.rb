f31b02ca3ecd142c022fd6a0e901ae31de48aabb
Umair <umairchaudhary6161@gmail.com>
2022-12-20 18:56:31 +0500

    Add the puts in the conversation channel

--- app/channels/conversations_channel.rb
+++ app/channels/conversations_channel.rb
@@ -1,6 +1,14 @@
 class ConversationsChannel < ApplicationCable::Channel
   def subscribed
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+    puts params
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
     if params[:conversation_id].present?
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      puts params[:conversation_id]
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
       stream_from "conversation_#{params[:conversation_id]}"
     else
       puts "conversation id is missing."
