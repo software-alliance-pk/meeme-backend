aeb333ab21a28c392ab7cf87c75c28d3f79a2cc0
Umair <umairchaudhary6161@gmail.com>
2022-12-26 18:37:55 +0500

    alteration done for notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ApplicationRecord
   belongs_to :conversation, optional: true
   belongs_to :message, optional: true
 
-  def self.create_push_notification
+  def self.create_push_notification(notification)
     require 'fcm'
     fcm_client = FCM.new(ENV['FIREBASE_SERVER_KEY'])
     puts (ENV['FIREBASE_SERVER_KEY'])
@@ -16,14 +16,14 @@ class Notification < ApplicationRecord
     puts "----------------------------------------------"
     puts "----------------------------------------------"
     options = { priority: 'high',
-                data: { notification: self, user_id: self.user_id,
-                notification: { body: self.body,
+                data: { notification: notification, user_id: notification.user_id,
+                notification: { body: notification.body,
                                 title: 'MEMEE App Notification',
-                                user_id: self.user_id,
+                                user_id: notification.user_id,
                 } }
     }
-    registration_ids = user.mobile_devices.pluck(:mobile_token)
-    if user.notifications_enabled?
+    registration_ids = notification.user.mobile_devices.pluck(:mobile_token)
+    if notification.user.notifications_enabled?
       registration_ids.each do |registration_id|
         puts fcm_client.send(registration_id, options)
         puts "----------------------------------------------"
