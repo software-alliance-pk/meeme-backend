4877b32f5d2228b288af7d52a600f277ec987205
Umair <umairchaudhary6161@gmail.com>
2022-12-26 19:47:44 +0500

    Notifications with title implemented

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,29 +1,23 @@
 class Notification < ApplicationRecord
-  # after_create :create_push_notification
+  after_create :create_push_notification
   enum status: { un_read: 0, read: 1 }
   enum alert: { enabled: 0, disabled: 1 }
   belongs_to :user, optional: true
   belongs_to :conversation, optional: true
   belongs_to :message, optional: true
 
-  def self.create_push_notification(notification)
+  def create_push_notification
     require 'fcm'
     fcm_client = FCM.new(ENV['FIREBASE_SERVER_KEY'])
-    puts (ENV['FIREBASE_SERVER_KEY'])
-    puts "----------------------------------------------"
-    puts "----------------------------------------------"
-    puts "----------------------------------------------"
-    puts "----------------------------------------------"
-    puts "----------------------------------------------"
     options = { priority: 'high',
-                data: { notification: notification, user_id: notification.user_id,
-                notification: { body: notification.body,
-                                title: 'MEMEE App Notification',
-                                user_id: notification.user_id,
-                } }
+                notification: { body: self.body,
+                                title: self.title,
+                                user_id: self.user_id,
+                }
     }
-    registration_ids = notification.user.mobile_devices.pluck(:mobile_token)
-    if notification.user.notifications_enabled?
+
+    registration_ids = user.mobile_devices.pluck(:mobile_token)
+    if user.notifications_enabled?
       registration_ids.each do |registration_id|
         puts fcm_client.send(registration_id, options)
         puts "----------------------------------------------"
@@ -31,8 +25,6 @@ class Notification < ApplicationRecord
         puts "----------------------------------------------"
         puts "----------------------------------------------"
         puts "----------------------------------------------"
-
-
       end
     end
   end
