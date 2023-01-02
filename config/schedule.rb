a710126b59c15e51cae72b5ddeb7e1d20d24c176
Umair <umairchaudhary6161@gmail.com>
2023-01-02 16:31:54 +0500

    update cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,18 @@
+# whenever --update-crontab --set environment='development' set the environemnt
+# whenever use to display schedule
+#  whenever --update-crontab update cron tab
+# crontab -r clean crontab
+
+
+
+
 set :output, "./log/cron.log"
-every 24.hours do
+every 1.minutes do
   runner "Story.delete_after_24_hours"
+end
+
+every 1.minutes do
+  runner "puts 'Task is running'"
+  runner 'puts Rails.env'
 end
 
