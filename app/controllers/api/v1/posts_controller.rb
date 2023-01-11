ee95d7228f9c0f237e3738b0ebf0cc560461dff7
Umair <umairchaudhary6161@gmail.com>
2023-01-11 20:20:09 +0500

    badge issue resolved

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -140,7 +140,7 @@ class Api::V1::PostsController < Api::V1::ApiController
       else
         count = @share_post.share_count + 1
         @share_post.update_columns(share_count: count)
-        ShareBadgeJob.perform_now(@share_post)
+        ShareBadgeJob.perform_now(@share_post,@current_user)
         render json: { message: 'Tournament Posts Shared', post: @share_post, share_count: @share_post.share_count }, status: :ok
       end
     else
--- app/jobs/share_badge_job.rb
+++ app/jobs/share_badge_job.rb
@@ -1,17 +1,16 @@
 class ShareBadgeJob < ApplicationJob
   queue_as :default
-  def perform(message)
+  def perform(message,current_user)
     @badge = Badge.find_by(title: "Sharing Is Caring Badge")
-    
-    @check = UserBadge.find_by(user_id: message.user_id, badge_id: @badge.id)
+    @check = UserBadge.find_by(user_id: current_user.id, badge_id: @badge.id)
     if @check.present?
-      puts "#{User.find_by(id: message.user_id).username} has already been awarded #{@badge.title}"
+      puts "#{User.find_by(id: current_user.id).username} has already been awarded #{@badge.title}"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
     else
-      @awarded_badge = UserBadge.create!(user_id: message.user_id, badge_id: @badge.id)
-      puts "Congratulations #{User.find_by(id: message.user_id).username} . You have been awarded #{@badge.title}"
+      @awarded_badge = UserBadge.create!(user_id: current_user.id, badge_id: @badge.id)
+      puts "Congratulations #{User.find_by(id: current_user.id).username} . You have been awarded #{@badge.title}"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
