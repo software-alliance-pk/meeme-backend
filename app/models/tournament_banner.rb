e185a5f3d16297291d40f4da860f2f652aeef118
FahadButt123 <fahad.butt@viralsquare.org>
2023-02-13 15:17:28 +0500

    Fixed the compressed file issue

--- app/models/tournament_banner.rb
+++ app/models/tournament_banner.rb
@@ -7,13 +7,13 @@ class TournamentBanner < ApplicationRecord
   has_one :ranking_price,dependent: :destroy
   has_one :tournament_banner_rule,dependent: :destroy
 
-  after_create_commit :compress
-
-  def compress
-    if self.post_image.present? && self.post_image.content_type[0...5] == "image"
-      @image = self.post_image.variant(quality: 45).processed.url
-      self.update(compress_image: @image)
-    end
-  end
+  # after_create_commit :compress
+  #
+  # def compress
+  #   if self.tournament_banner_photo.present? && self.tournament_banner_photo.content_type[0...5] == "image"
+  #     @image = self.tournament_banner_photo.variant(quality: 45).processed.url
+  #     self.update(compress_image: @image)
+  #   end
+  # end
 
 end
