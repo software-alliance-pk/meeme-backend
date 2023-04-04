04945e11753b3adf9286a49b93318856a04443a1
FahadButt123 <fahad.butt@viralsquare.org>
2023-04-04 17:21:41 +0500

    bug fixed

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,3 +1,4 @@
 class Tutorial < ApplicationRecord
+  validates :step, uniqueness: true
   has_many_attached :tutorial_images
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+tutorials = { 1 => ["Memee will have a monthly Tournament that users can join."],
+              2 => ["The Tournament starts every first day of the month and winners are announced during the last day of each month."],
+              3 => ["End Users can join and post  memes in the tournament as many  as they want."],
+              4 => ["There are also other ways to accumulate coins. Judging is another way of acquiring coins from the app. End Users can  judge 100 memes daily and gain 50 coins on a daily basis."],
+              5 => ["Coins can buy icons, themes, profile and background overlays."],
+              6 => ["There are hundreds of combination that you can choose from to make a unique experience using the app."],
+              7 => ["The top 3 votes are the tournament winners and will be rewarded with Amazon gift cards."],
+              8 => ["The 4th to 10th place in the tournament will receive coin rewards."] }
+
+tutorials.keys.each do |key|
+  tutorials[key].each do |value|
+    @images = Dir.glob("app/assets/images/tutorials/step#{key}/*.png")
+    t = Tutorial.create!(
+      description: value,
+      step: key,
+      images: @images
+    )
+
+    t.images.each do |image|
+      t.tutorial_images.attach(
+        io: File.open(File.join(Rails.root, image)),
+        filename: "tutorials.png"
+      )
+    end
+  end
+end
+
 badges = { 0 => ["Thousand Miles Badges",
                  "Master Aficionado Badge",
                  "Opinionated Master Badge",
@@ -46,33 +73,6 @@ badges.keys.each do |keys|
       io: File.open(File.join(Rails.root, "app/assets/images/badges/#{values}.png")),
       filename: "#{values}.png"
     )
-  end
-end
-
-tutorials = { 1 => ["Memee will have a monthly Tournament that users can join."],
-              2 => ["The Tournament starts every first day of the month and winners are announced during the last day of each month."],
-              3 => ["End Users can join and post  memes in the tournament as many  as they want."],
-              4 => ["There are also other ways to accumulate coins. Judging is another way of acquiring coins from the app. End Users can  judge 100 memes daily and gain 50 coins on a daily basis."],
-              5 => ["Coins can buy icons, themes, profile and background overlays."],
-              6 => ["There are hundreds of combination that you can choose from to make a unique experience using the app."],
-              7 => ["The top 3 votes are the tournament winners and will be rewarded with Amazon gift cards."],
-              8 => ["The 4th to 10th place in the tournament will receive coin rewards."] }
-
-tutorials.keys.each do |key|
-  tutorials[key].each do |value|
-    @images = Dir.glob("app/assets/images/tutorials/step#{key}/*.png")
-    t = Tutorial.create!(
-      description: value,
-      step: key,
-      images: @images
-    )
-
-    t.images.each do |image|
-      t.tutorial_images.attach(
-        io: File.open(File.join(Rails.root, image)),
-        filename: "tutorials.png"
-      )
-    end
   end
 end
 
