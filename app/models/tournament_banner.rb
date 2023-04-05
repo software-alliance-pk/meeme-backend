9a45d07abd080e154888a64011160fa3412c6aa9
FahadButt123 <fahad.butt@viralsquare.org>
2023-04-05 14:28:33 +0500

    Dependents Destroy

--- app/models/tournament_banner.rb
+++ app/models/tournament_banner.rb
@@ -1,8 +1,8 @@
 class TournamentBanner < ApplicationRecord
   has_many :tournament_users, dependent: :destroy
   has_many :users, through: :tournament_users
-  has_many :posts
-  has_many :likes
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_one_attached :tournament_banner_photo ,dependent: :destroy
   has_one :ranking_price,dependent: :destroy
   has_one :tournament_banner_rule,dependent: :destroy
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,4 +1,4 @@
 class Tutorial < ApplicationRecord
   validates :step, uniqueness: true
-  has_many_attached :tutorial_images
+  has_many_attached :tutorial_images, dependent: :destroy
 end
