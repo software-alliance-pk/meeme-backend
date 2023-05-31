5f7a279b5a12314d66d757109297ed938ed66949
FahadButt123 <fahad.butt@viralsquare.org>
2023-05-31 21:34:54 +0500

    CLoudfront Url Added

--- app/services/cloudfront_url_service.rb
+++ app/services/cloudfront_url_service.rb
@@ -5,17 +5,17 @@ class CloudfrontUrlService
   end
 
   def cloudfront_url
-    url = "https://d3mh1peks7kf2k.cloudfront.net/#{@image.blob.key}"
+    url = "https://d28n2fi547yxhh.cloudfront.net/#{@image.blob.key}"
     url
   end
 
   def generate_thumbnail
-    url = "https://d3mh1peks7kf2k.cloudfront.net/#{@image}"
+    url = "https://d28n2fi547yxhh.cloudfront.net/#{@image}"
     url
   end
 
   def admin_urls
-    url = "https://d3mh1peks7kf2k.cloudfront.net/#{@image.key}"
+    url = "https://d28n2fi547yxhh.cloudfront.net/#{@image.key}"
     url
   end
 end
