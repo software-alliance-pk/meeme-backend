f321aab37d2e69ebd5304e8bfca21dfa2906d117
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-03-11 15:22:40 +0500

    pull from staging

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :secret_key => "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",
-  :publishable_key => "pk_live_51LiJD6GTUIZsigd59dvkHtC2tgQMYr61Hu7wzCvEGFAUoNprt40lGGk9hoK3d6PaW7DJGiQuhgl0qAKgfQeNZTcB00Q5dyZtMK"
+  :secret_key => ENV['STRIPE_SECRET_KEY'],
+  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
