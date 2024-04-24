108ec032cb80a566e37284ac9d4a5ac027a0c7d8
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-04-24 16:29:58 +0500

    PEER changes in redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'], size: 12, network_timeout: 10,
-                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER }
   }
   Redis.exists_returns_integer = false
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'], size: 1, network_timeout: 10,
-                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+                   ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER }
   }
   Redis.exists_returns_integer = false
 end
