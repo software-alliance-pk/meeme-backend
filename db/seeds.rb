1a9cf548a3d76704cb3cb3d0663a3a31e8a3d7a1
Aqsa <aqsajabbar7689@gmail.com>
2024-06-12 13:02:25 +0500

    Amazon card commented in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -173,9 +173,9 @@ end
 # end
 
 # Clear existing records to avoid duplication if needed
-AmazonCard.destroy_all
+# AmazonCard.destroy_all
 # Create 3 static Amazon gift cards
-AmazonCard.create(amount: 10, coin_price: 120000)
-AmazonCard.create(amount: 25, coin_price: 300000)
-AmazonCard.create(amount: 50, coin_price: 600000)
-puts "Amazon gift cards seeded successfully!"
+# AmazonCard.create(amount: 10, coin_price: 120000)
+# AmazonCard.create(amount: 25, coin_price: 300000)
+# AmazonCard.create(amount: 50, coin_price: 600000)
+# puts "Amazon gift cards seeded successfully!"
