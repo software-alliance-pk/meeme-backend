fa7a7578e035a8732e81ce212927a9de35adcf68
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-07-03 18:03:07 +0500

    changes

--- app/controllers/api/v1/amazon_cards_controller.rb
+++ app/controllers/api/v1/amazon_cards_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AmazonCardsController < Api::V1::ApiController
     before_action :authorize_request
   
     def index
-      @amazon_cards = AmazonCard.all.order( 'coin_price ASC' )
+      @amazon_cards = AmazonCard.all.order( 'amount ASC' )
       render json: @amazon_cards
     end
   
