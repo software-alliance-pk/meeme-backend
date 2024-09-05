fde8b263a55f7fdd8ba8a2f135fad53af516d198
Aqsa <aqsajabbar7689@gmail.com>
2024-09-05 12:07:40 +0500

    destoy user token when disabled

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -9,11 +9,17 @@ class Api::V1::ApiController < ActionController::API
   def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
-
-    if VerificationToken.find_by_token(header).present?
+    verification_token = VerificationToken.find_by_token(header)
+    if verification_token.present?
       begin
         @decoded = JsonWebTokenService.decode(header)
         @current_user = User.find_by_id(@decoded[:user_id]) || User.find_by_email(@decoded[:email])
+      if @current_user&.disabled
+        verification_token.destroy
+        render json: { message: 'Your account has been disabled. Please contact the administrator.' }, status: :forbidden
+      else
+        @current_user
+      end
       rescue ActiveRecord::RecordNotFound => e
         render json: { message: 'Invalid Authentication' }, status: :unauthorized
       rescue JWT::DecodeError => e
