9338efc3b4a22c6d9dddc51dd6d203ca02d98f8b
Aqsa <aqsajabbar7689@gmail.com>
2024-10-29 12:47:12 +0500

    admin user api

--- config/routes.rb
+++ config/routes.rb
@@ -105,6 +105,7 @@ Rails.application.routes.draw do
       get '/coin_prices', to: 'coin_prices#index'
       resources :users do
         collection do
+          get :get_admin_user
           post :delete_user
           get :search
           put :update_user
