b57c9ca43b2369e08d7e41b5ebc2a0048c228b6b
Haseeb71 <71abdulhaseeb71@gmail.com>
2025-02-20 21:19:49 +0500

    dsa

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -447,17 +447,20 @@ class Api::V1::PostsController < Api::V1::ApiController
     @following = User.where(id: @following).all
 
     @following.each do |user|
-      user_posts = user.posts.where(tournament_meme: false)
+      user.posts.where(tournament_meme: false).each do |post|
+        if post.flagged_by_user.include?(@current_user.id) || @current_user.blocked_users.pluck(:blocked_user_id).include?(post.user.id)
+
+      # user_posts = user.posts.where(tournament_meme: false)
       
-      # Filter posts based on the 'created_at' parameter if present
-      if params[:created_at].present?
-        created_at = Time.zone.parse(params[:created_at]) rescue nil
-        if created_at
-          user_posts = user_posts.where('posts.created_at <= ?', created_at) # Specify the table for created_at
-        else
-          render json: { message: "Invalid created_at format" }, status: :bad_request and return
-        end
-      end
+      # # Filter posts based on the 'created_at' parameter if present
+      # if params[:created_at].present?
+      #   created_at = Time.zone.parse(params[:created_at]) rescue nil
+      #   if created_at
+      #     user_posts = user_posts.where('posts.created_at <= ?', created_at) # Specify the table for created_at
+      #   else
+      #     render json: { message: "Invalid created_at format" }, status: :bad_request and return
+      #   end
+    end
 
       user_posts.each do |post|
         unless post.flagged_by_user.include?(@current_user.id) || @current_user.blocked_users.pluck(:blocked_user_id).include?(post.user.id)
