328b8d19360cb881639728063fcb075ae666d0dd
Aqsa <aqsajabbar7689@gmail.com>
2025-02-21 13:07:29 +0500

    following badge

--- app/jobs/following_badge_job.rb
+++ app/jobs/following_badge_job.rb
@@ -3,35 +3,35 @@ class FollowingBadgeJob < ApplicationJob
 
   def perform(message)
     current_user = User.find_by(id: message.follower_user_id)
-    if message.user.followings.following_added.count == 100
+    if current_user.followings.following_added.count == 20
       @badge = Badge.find_by(title: "Follower Bronze")
-      @check = UserBadge.find_by(user_id: message.user_id, badge_id: @badge.id)
+      @check = UserBadge.find_by(user_id: current_user.id, badge_id: @badge.id)
       if @check.present?
       else
-        @awarded_badge = UserBadge.create!(user_id: message.user_id, badge_id: @badge.id)
-        puts "Congratulations #{message.user.username} . You have been awarded #{@badge.title}"
+        @awarded_badge = UserBadge.create!(user_id: current_user.id, badge_id: @badge.id)
+        puts "Congratulations #{current_user.username} . You have been awarded #{@badge.title}"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       end
-    elsif message.user.followings.following_added.count == 5000
+    elsif current_user.followings.following_added.count == 5000
       @badge = Badge.find_by(title: "Follower Silver")
-      @check = UserBadge.find_by(user_id: message.user_id, badge_id: @badge.id)
+      @check = UserBadge.find_by(user_id: current_user.id, badge_id: @badge.id)
       if @check.present?
       else
-        @awarded_badge = UserBadge.create!(user_id: message.user_id, badge_id: @badge.id)
-        puts "Congratulations #{message.user.username} . You have been awarded #{@badge.title}"
+        @awarded_badge = UserBadge.create!(user_id: current_user.id, badge_id: @badge.id)
+        puts "Congratulations #{current_user.username} . You have been awarded #{@badge.title}"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
       end
-    elsif message.user.followings.following_added.count == 100000
+    elsif current_user.followings.following_added.count == 100000
       @badge = Badge.find_by(title: "Follower Gold")
-      @check = UserBadge.find_by(user_id: message.user_id, badge_id: @badge.id)
+      @check = UserBadge.find_by(user_id: current_user.id, badge_id: @badge.id)
       if @check.present?
       else
-        @awarded_badge = UserBadge.create!(user_id: message.user_id, badge_id: @badge.id)
-        puts "Congratulations #{message.user.username} . You have been awarded #{@badge.title}"
+        @awarded_badge = UserBadge.create!(user_id: current_user.id, badge_id: @badge.id)
+        puts "Congratulations #{current_user.username} . You have been awarded #{@badge.title}"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
         puts ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
