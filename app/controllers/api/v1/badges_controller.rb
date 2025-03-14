6590182aa6c8075e0c9478e0d24a5a92bc25863b
Aqsa <aqsajabbar7689@gmail.com>
2025-03-14 15:34:47 +0500

    judge bronze

--- app/controllers/api/v1/badges_controller.rb
+++ app/controllers/api/v1/badges_controller.rb
@@ -69,7 +69,6 @@ class Api::V1::BadgesController < Api::V1::ApiController
     @result = []
     @today_date = Time.zone.now.end_of_day.to_datetime
     60.times do |num|
-      debugger
       status = Like.where(created_at: (@today_date - num).beginning_of_day..(@today_date - num).end_of_day, is_judged: true, user_id: @current_user.id).where.not(post_id: nil).present?
       @result << status
       if status
