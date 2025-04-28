ade49da41edc96e3c5fc7f11d6bbd7afb05d3952
Aqsa <aqsajabbar7689@gmail.com>
2025-04-28 13:13:33 +0500

    coin prices api issue resolved

--- app/controllers/api/v1/coin_prices_controller.rb
+++ app/controllers/api/v1/coin_prices_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class CoinPricesController < ApplicationController
+      skip_before_action :authenticate_admin_user!
       def index
         coin_prices = CoinPrice.all
         render json: coin_prices
